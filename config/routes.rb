4c988fe87264abf70dd5055ecb5bf5b92349e54c
Emmanuel San Miguel <esanmiguel@gmail.com>
2023-05-08 22:36:41 -0500

    Styles the index page with input

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get "weather/index"
-  get "weather/:zip", to: "weather#show"
+  get "weather/:zip", to: "weather#show", as: "show_weather"
 
   root "weather#index"
 end
