772d282a4524918f612d7b1e54f7814ba9f7cb83
Emmanuel San Miguel <esanmiguel@gmail.com>
2023-05-09 10:39:11 -0500

    Add comment and final translation

--- app/services/address_validator.rb
+++ app/services/address_validator.rb
@@ -1,5 +1,6 @@
 class AddressValidator
   def self.validate(address)
+    # We check if the address has a 5-digit string and consider that the zip
     zip = address.scan(/\b\d{5}\b/).first
 
     return Result.fail(:invalid_address) unless zip
